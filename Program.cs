5d9c78cae65348440790456bbe77578bad643f86
Gabriel Bolonhez <g.felipe@intertox.com.br>
2025-09-13 17:23:10 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@test.com" && loginDTO.Password == "123456")
+    {
+        return Results.Ok("Login successfully");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
+
+app.Run();
+
+public class LoginDTO
+{
+    public string Email { get; set; } = default!;
+
+    public string Password { get; set; } = default!;
+
+
+}
+

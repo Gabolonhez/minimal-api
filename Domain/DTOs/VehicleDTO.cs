53cfffa26bec41878ddb09654164fbfe7c11fd75
Gabriel Bolonhez <g.felipe@intertox.com.br>
2025-09-18 23:57:34 -0300

    feat: adding vehicle CRUD

---
+++ Domain/DTOs/VehicleDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace minimal_api.Domain.DTOs
+{
+    public record class VehicleDTO
+    {
+        public string Name { get; set; } = default!;
+        public string Brand { get; set; } = default!;
+        public int Year { get; set; } = default!;
+    }
+}
